6b58f324cf381d822dce6f54060e89ae67bf05f2
Omer Kilinc <omer.emre.kilinc@outlook.com>
2022-04-14 17:00:35 +0400

    Added Functions to vectors.cpp
    Added Vector addition, subtracting, multiplication (dot product, cross product, with scalar) and magnitude

--- calculations/vectors.cpp
+++ calculations/vectors.cpp
@@ -1,28 +1,61 @@
-class Vector {
+// TODO Shorten code with direct return values eg: return x2*y2; instead of x=x2; y=y2; return x*y;
+
+
+class oVector {
 public:
-    Vector (float x2, float y2, float z2) {
+    oVector (float x2, float y2, float z2) {
         x = x2;
         y = y2;   
-        z = z2;
-         
+        z = z2; 
     }   
     
-    Vector vAdd (Vector v1, Vector v2) {
+    oVector vAdd (oVector v1, oVector v2) {
         x = v1.x + v2.x;
         y = v1.y + v2.y;
         z = v1.z + v2.z;
-        Vector v3 (x, y, z);
+        oVector v3 (x, y, z);
         return v3;
     }
 
-    Vector vSub (Vector v1, Vector v2) {
+    oVector vSub (oVector v1, oVector v2) {
         x = v1.x - v2.x;
         y = v1.y - v2.y;
         z = v1.z - v2.z;
-        Vector v3 (x, y, z);
+        oVector v3 (x, y, z);
+        return v3;
     }
 
-    
+    oVector vMultScalarVector (float scalar, oVector v1) {
+        x = scalar * v1.x;
+        y = scalar * v1.y;
+        z = scalar * v1.z;
+        oVector v3 (x, y, z);
+        return v3;
+    }
+
+    float vDotProduct (oVector v1, oVector v2) {
+        x = v1.x * v2.x;
+        y = v1.y * v2.y;
+        z = v1.z * v2.z;
+        return x + y + z;
+    }    
+
+    oVector vCrossProduct (oVector v1, oVector v2) {
+        x = v1.y * v2.z - v1.z * v2.y;
+        y = v1.z * v2.x - v1.x * v2.z;
+        z = v1.x * v2.y - v1.y * v2.x;
+        oVector v3 (x, y, z);
+        return v3;
+    }
+
+    float vMagnitude (oVector v1) {
+        x = v1.x * v1.x;
+        y = v1.y * v1.y;
+        z = v1.z * v1.z;
+        return sqrt(x + y + z);
+    }
+
+
 
 private:
     float x;
--- openmath.hpp
+++ openmath.hpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include "calculations/vectors.cpp"
 #include "calculations/solve.cpp"
 #include "calculations/calculus.cpp"
